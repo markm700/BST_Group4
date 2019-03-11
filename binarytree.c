2d86f225bffe8d34d676aad1865fa00b3fc2da15
MarkMatas <mm249676@umconnect.umt.edu>
2019-03-11 14:49:59 -0600

    coded the search and insert functions from the powerpoint

--- binarytree.c
+++ binarytree.c
@@ -10,15 +10,53 @@ void initialize(BT* bt){
 }
 
 bool search(BT* bt, int key){
-	
+	Node* current=bt->root;
+	while(current!=NULL){
+		if(key==current->data){
+			return true;
+		}
+		else if(key<current->data){
+			current=current->left;
+		}
+		else{
+			current=current->right;
+		}
+	}
+	return false; //key not found
 }
 
 void insert(BT* bt, int item){
-	
+	Node* temp=newNode(item);
+	if(bt->root==NULL){
+		bt->root=temp;
+	}
+	else{
+		Node* current=bt->root;
+		while(current!=NULL){
+			if(temp->data < current->data){
+				if(current->left==NULL){
+					current->left=temp;
+					current=NULL;
+				}
+				else{
+					current=current->left;
+				}
+			}
+			else{
+				if(current->right==NULL){
+					current->right=temp;
+					current=NULL;
+				}
+				else{
+					current=current->right;
+				}
+			}
+		}
+	}
 }
 
 void printinorder(BT* bt){
-	
+	Node currNode=bt->root;
 }
 
 void printpreorder(BT* bt){
